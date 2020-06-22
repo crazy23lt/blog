f5bef5dec5ed1839a748cdef11f18b1e921d37a7
Mr.liu <2394630102@qq.com>
2020-06-22 17:24:56 +0800

    添加脚本文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,9 @@ const markdown = require('./config/markdown')
 const plugins = require('./config/plugins')
 const themeConfig = require('./config/theme')
 module.exports = {
-  dest: 'blog', // 指定 vuepress build 的输出目录
+  base: /blog/,
+  repo: 'https://github.com/crazy23lt/blog.git',
+  repoLabel: 'My GitHub',
   theme: 'reco',  // 引用主题
   title: "Web学习之路", // 网站的标题
   description: '生命以负熵为食',  // 网站描述
